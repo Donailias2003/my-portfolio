74d3b382e4022a49a5113c39f9a3d084c0eb31e5
Ilias <93838095+Donailias2003@users.noreply.github.com>
2023-04-18 15:22:25 +0200

    Upd site

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -47,11 +47,9 @@ const About = () => {
         variants={fadeIn("", "", 0.1, 1)}
         className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
       >
-        I'm a skilled software developer with experience in TypeScript and
-        JavaScript, and expertise in frameworks like React, Node.js, and
-        Three.js. I'm a quick learner and collaborate closely with clients to
-        create efficient, scalable, and user-friendly solutions that solve
-        real-world problems. Let's work together to bring your ideas to life!
+      I am a computer science school student where I learned many programming languages like: java, 
+      .NET Core, PHP, JavaScript, C#, basic Python. I learned many algorithmic problem solving techniques 
+      and developed many prototypes using the listed languages.
       </motion.p>
 
       <div className="mt-20 flex flex-wrap gap-10">
