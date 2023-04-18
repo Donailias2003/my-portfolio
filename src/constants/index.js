adb1ec753546917d7daa9cc76ff69434061e2afb
Ilias <93838095+Donailias2003@users.noreply.github.com>
2023-04-18 15:47:30 +0200

    upd

--- src/constants/index.js
+++ src/constants/index.js
@@ -116,16 +116,16 @@ const technologies = [
 
 const experiences = [
   {
-    title: "React.js Developer",
-    company_name: "Starbucks",
+    title: "Informatico di gestione AFC",
+    company_name: "Scuola Specializzata Superiore Informatica di Gestione",
     icon: starbucks,
     iconBg: "#383E56",
-    date: "March 2020 - April 2021",
+    date: "Sep 2021 - Prensent",
     points: [
-      "Developing and maintaining web applications using React.js and other related technologies.",
-      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-      "Implementing responsive design and ensuring cross-browser compatibility.",
-      "Participating in code reviews and providing constructive feedback to other developers.",
+      "Development of management apps.",
+      "Database creation with Oracle, MySql, SQL Server.",
+      "Mobile and desktop application development.",
+      "Accounting, economics, statistics, algorithmics, mathematics, etc...",
     ],
   },
   {
